63203eec01ca06b0f7e9623a8331863e7ef8d2e4
Akash Wagh <Akash.Wagh@esds.co.in>
2026-06-26 04:40:09 +0530

    Enhance changes new

--- config/cors.php
+++ config/cors.php
@@ -17,23 +17,26 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    // 'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter([
-        env('FRONTEND_URL', 'http://localhost:5173'),
-        'http://localhost:5173',
-        'http://localhost:3000',
-        'https://autowave-bid.playltp.in',
-        'https://bid.playltp.in',
-    ]),
+    // 'allowed_origins' => array_filter([
+    //     env('FRONTEND_URL', 'http://localhost:5173'),
+    //     'http://localhost:5173',
+    //     'http://localhost:3000',
+    //     'https://autowave-bid.playltp.in',
+    //     'https://bid.playltp.in',
+    // ]),
+
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
-    'max_age' => 86400,
+    'max_age' => 0,
 
     'supports_credentials' => true,
 
